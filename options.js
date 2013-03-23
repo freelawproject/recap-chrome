99e9f1ea4093e753bee15dd595bfc355cdd8c813
Ka-Ping Yee <ping@zesty.ca>
2013-03-23 16:54:54 -0700

    Tidy up the options popup; remove the "Save" button; add an "About" link.

--- options.js
+++ options.js
@@ -2,17 +2,18 @@ var inputs = document.getElementsByTagName('input');
 function load_options() {
   chrome.storage.sync.get('options', function (items) {
     for (var i = 0; i < inputs.length; i++) {
-      inputs[i].checked = items.options[inputs[i].name];
+      inputs[i].checked = items.options[inputs[i].id];
     }
   });
 }
 function save_options() {
   var options = {};
   for (var i = 0; i < inputs.length; i++) {
-    options[inputs[i].name] = inputs[i].checked;
+    options[inputs[i].id] = inputs[i].checked;
   }
   chrome.storage.sync.set({options: options});
 }
-document.getElementById('save').addEventListener('click', save_options);
-document.getElementById('cancel').addEventListener('click', load_options);
 load_options();
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener('change', save_options);
+}
