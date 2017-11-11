45bbfb563ed08aa028f375dcc206e82c4c3210bd
Michael Lissner <mlissner@michaeljaylissner.com>
2017-11-10 17:34:46 -0800

    Fixes bug with setting default options. Should be much better now.

--- background.js
+++ background.js
@@ -1,26 +1,32 @@
-chrome.storage.local.get('options', function (items) {
-  // Set options to their default values.
-  // Check each default value. If it's not already in the options, set it to
-  // the default.
-  let defaults = {
-    recap_link_popups: true,
-    show_notifications: true,
-    recap_disabled: false,
-
-    // Radio button
-    ia_style_filenames: false,
-    lawyer_style_filenames: true,
-  };
-  for (let key in defaults) {
-    if (!(key in items.options)){
-      items.options[key] = defaults[key];
-    }
-  }
-  chrome.storage.local.set({options: items.options});
-});
-
 // Make services callable from content scripts.
 exportInstance(Notifier);
 exportInstance(ToolbarButton);
 exportInstance(Recap);
 
+function setDefaultOptions(details) {
+  // Set options to their default values.
+  chrome.storage.local.get('options', function (items) {
+    let defaults = {
+      recap_link_popups: true,
+      show_notifications: true,
+      recap_disabled: false,
+
+      // Radio button
+      ia_style_filenames: false,
+      lawyer_style_filenames: true,
+    };
+    if (!items.options) {
+      // Brand new install. Use the defaults.
+      items.options = defaults;
+    } else {
+      // Existing install. Set any new defaults.
+      for (let key in defaults) {
+        if (!(key in items.options)) {
+          items.options[key] = defaults[key];
+        }
+      }
+    }
+    chrome.storage.local.set({options: items.options});
+  });
+}
+chrome.runtime.onInstalled.addListener(setDefaultOptions);
