94e7b7983b643260f49849f84992112f3b872038
Travis Briggs <audiodude@gmail.com>
2018-01-07 21:53:50 -0800

    Do not capture client output in Karma

--- karma.conf.js
+++ karma.conf.js
@@ -62,6 +62,10 @@ module.exports = function(config) {
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
+    client: {
+      captureConsole: false
+    },
+
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
