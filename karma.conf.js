4943476d7209c28fd50022faef3dd1c3fbfec67b
Brad Johnson <cdale77@gmail.com>
2018-03-16 16:29:13 -0700

    fix(tests): lengthen karma timeout

--- karma.conf.js
+++ karma.conf.js
@@ -74,6 +74,7 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
+    browserNoActivityTimeout: 210000,
 
     customLaunchers: {
       Chrome_CI: {
