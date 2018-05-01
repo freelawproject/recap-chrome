a8b12b881eea395755011ced73da78519a9a202d
Mike Lissner <mike@free.law>
2018-05-01 09:54:15 -0700

    Restores karma file

--- karma.conf.js
+++ karma.conf.js
@@ -81,6 +81,7 @@ module.exports = function(config) {
         flags: [
           '--no-sandbox',
           '--disable-gpu',
+          '--headless',
           '--remote-debugging-port=9222',
         ]
       }
