18fd688fd8f4f7c22c0a953e4b0314e35eecf756
John Hawkinson <jhawk@mit.edu>
2019-01-24 12:25:02 -0500

    2019.01.24.restricted warnings (#51)
    * isSingleDocumentPage: initial cut of restriction detection
    in content_delegate
    
    Paranoid.
    Also runs twice.
    
    xxx needs to be a function
    
    * restrict: check for both -> either
    
    * functionalize checkRestrictions()
    
    * Move this.restricted check into showPdfPage()
    
    * content_delegate: rewrite xxx comment about changing icon
    
    * dev -> production. Convert alert() to console.log()
    
    * checkRestrictions: add some sealing strings
    
    * checkRestrictions() Add boxed annotaiton
    
    Add boxed recap-banner style annoation to the page,
    using insertAdjacnetHTML() [which is somehow still allowed].
    
    * checkRestrictions(): Display disabled icon next to annotation
    
    I accept criticism on the css, because css is not my thing.
    
    We are now centering with a pair of <div>s that are text-align: center
    and display: inline-block, and then left/right aligning with another
    pair of divs that are display: table and display: table-cell, with
    padding. There is likely a better way to do this?
    
    Also, perhaps some of this should make it into the stylesheet, but it
    is not yet apparent what?
    
    * checkRestrictions(): wordsmith annotation
    
    "that is restricted" -> "that may be restricted"
    
    * typo in last
    
    "that may be restricted" -> "that it may be restricted"

--- content_delegate.js
+++ content_delegate.js
@@ -18,7 +18,93 @@ let ContentDelegate = function(url, path, court, pacer_case_id, pacer_doc_id,
   this.recap = importInstance(Recap);
 
   this.findAndStorePacerDocIds();
+
+  this.restricted = this.checkRestrictions();
 };
+
+// Check for document restrictions
+ContentDelegate.prototype.checkRestrictions = function() {
+  // Some documents are restricted to case participants. Typically
+  // this is offered with either an interstitial page (in the case
+  // of free looks) or an extra box on the receipt page. In both cases
+  // it's something like this:
+  //
+  // <table><tbody>
+  //   <tr><td>Warning!</td></tr>
+  //   <tr><td><b>This document is restricted to court users,
+  //              case participants and public terminal users.</b></td></tr>
+  // </tbody></table>
+  //
+  // The exact text will change, depending on the circumstances. For
+  // sealed documents, e.g., ohsd offers:
+  //
+  //   "The document you are about to view is SEALED; do not allow it
+  //   to be seen by unauthorized persons."
+  //
+  // Sealing behavior differs from CMECF instance to CMECF instance.
+  //
+  // Be somewhat paranoid about this and check for either a "Warning!"
+  // in the first <td> cell of a table, as well as any <b> containing
+  // "document is restricted", "SEALED", or "do not allow it to be seen".
+  // Case-insensitively.
+
+  let restrictedDoc = false;
+
+  for (let td of
+       document.querySelectorAll("table td:first-child")) {
+    if (td.textContent.match(/Warning!/)) {
+      restrictedDoc = true;
+      break;
+    }
+  }
+
+  for (let td of document.querySelectorAll("b")) {
+    if (td.textContent.match(
+	/document is restricted|SEALED|do not allow it to be seen/i
+    )) {
+        restrictedDoc = true;
+      break;
+    }
+  }
+
+  if (restrictedDoc) {
+    console.log("RECAP: Restricted document detected. Skipping upload.");
+    // We would like to alter the [R] icon to indicate what's going
+    // on, but we cannot call chrome.browserAction.setIcon()
+    // here. Instead, we'd need to send a message to the background
+    // script? ughhhh. Punt for now.
+
+    // Insert a RECAP banner near the end of the form, before the action button.
+    // Ideally this would have some RECAP branding, icon/logo, etc.
+
+    // Ideally we target the form <input>, but absent that
+    // we just go to the end of the final form.
+    // Should we just always go the end of the final form?
+    let target =
+	document.querySelector("form input") ||
+	document.forms[document.forms.length - 1].lastChild;
+
+    // Nested div for horizontal centering.
+    target.insertAdjacentHTML('beforebegin',
+      `<div style="text-align: center">
+	  <div style="display: inline-block; text-align: left; align: top">
+            <div class="recap-banner" style="display: table">
+              <div style="display: table-cell; padding: 12px; ">
+		<img src="${chrome.extension.getURL('assets/images/disabled-38.png')}"
+		     style="width: auto; height: auto">
+	      </div>
+	      <div style="display: table-cell; vertical-align: middle">
+		This document <b>will not be uploaded</b> to the RECAP Archive
+		because the RECAP extension has detected that it may be
+		restricted from public distribution.
+	      </div>
+	    </div>
+          </div>
+	</div>`);
+  }
+
+  return restrictedDoc;
+}
 
 // Use a variety of approaches to get and store pacer_doc_id to pacer_case_id
 // mappings in local storage.
@@ -370,7 +456,7 @@ ContentDelegate.prototype.showPdfPage = function(
         chrome.storage.local.get('options', displayPDF);
 
         let uploadDocument = function(items){
-          if (!items['options']['recap_disabled']) {
+          if (!items['options']['recap_disabled'] && !this.restricted) {
             // If we have the pacer_case_id, upload the file to RECAP.
             // We can't pass an ArrayBuffer directly to the background
             // page, so we have to convert to a regular array.
