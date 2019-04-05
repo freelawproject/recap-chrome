234e828d7839781887a84b679b77941128f180d8
Jason Aller <jraller@gmail.com>
2019-04-04 20:04:50 -0700

    test(ContentDelegateSpec.js): add tests for handleAttachmentMenuPage

--- spec/ContentDelegateSpec.js
+++ spec/ContentDelegateSpec.js
@@ -317,88 +317,140 @@ describe('The ContentDelegate class', function() {
   });
 
   describe('handleAttachmentMenuPage', function() {
-    let form;
-    beforeEach(function() {
-      form = document.createElement('form');
-      document.body.appendChild(form);
-      window.chrome = {
-        storage : {
-          local : {
-            get : jasmine.createSpy().and.callFake(function(
-                _, cb) { cb({options : {}}); })
-          }
-        }
-      };
-    });
-
-    afterEach(function() {
-      form.remove();
-      delete window.chrome;
-    });
-
-    describe('when there is NO appropriate form', function() {
-      it('has no effect when the URL is wrong', function() {
-        const cd = nonsenseUrlContentDelegate;
-        spyOn(cd.recap, 'uploadAttachmentMenu');
-        cd.handleAttachmentMenuPage();
-        expect(cd.recap.uploadAttachmentMenu).not.toHaveBeenCalled();
-      });
-
-      it('has no effect with a proper URL', function() {
-        const cd = singleDocContentDelegate;
-        spyOn(cd.recap, 'uploadAttachmentMenu');
-        cd.handleAttachmentMenuPage();
-        expect(cd.recap.uploadAttachmentMenu).not.toHaveBeenCalled();
-      });
-    });
-
-    describe('when there IS an appropriate form', function() {
+    describe('option disabled', function() {
+      let form;
       let input;
+
       beforeEach(function() {
+        form = document.createElement('form');
+        document.body.appendChild(form);
+        window.chrome = {
+          storage : {
+            local : {
+              get : jasmine.createSpy().and.callFake(function(
+                _, cb) { cb({options : {recap_disabled: true}}); })
+            }
+          }
+        };
         input = document.createElement('input');
         input.value = 'Download All';
         form.appendChild(input);
       });
 
-      it('has no effect when the URL is wrong', function() {
-        const cd = nonsenseUrlContentDelegate;
+      afterEach(function() {
+        form.remove();
+        delete window.chrome;
+      });
+
+      it('has no effect recap_disabled option is set', function() {
+        const cd = singleDocContentDelegate;
         spyOn(cd.recap, 'uploadAttachmentMenu');
         cd.handleAttachmentMenuPage();
         expect(cd.recap.uploadAttachmentMenu).not.toHaveBeenCalled();
       });
+    });
 
-      it('uploads the page when the URL is right', function() {
-        const cd = singleDocContentDelegate;
-        spyOn(cd.recap, 'uploadAttachmentMenu');
-        cd.handleAttachmentMenuPage();
-        expect(cd.recap.uploadAttachmentMenu).toHaveBeenCalled();
+    describe('option enabled', function () {
+      let form;
+      beforeEach(function() {
+        form = document.createElement('form');
+        document.body.appendChild(form);
+        window.chrome = {
+          storage : {
+            local : {
+              get : jasmine.createSpy().and.callFake(function(
+                  _, cb) { cb({options : {}}); })
+            }
+          }
+        };
       });
 
-      it('calls the upload method and responds to positive result', function() {
-        const cd = singleDocContentDelegate;
-        const uploadFake = function(pc, pci, h, callback) { callback(true); };
-        spyOn(cd.recap, 'uploadAttachmentMenu').and.callFake(uploadFake);
-        spyOn(cd.notifier, 'showUpload');
-        spyOn(history, 'replaceState');
-
-        cd.handleAttachmentMenuPage();
-        expect(cd.recap.uploadAttachmentMenu).toHaveBeenCalled();
-        expect(cd.notifier.showUpload).toHaveBeenCalled();
-        expect(history.replaceState)
-            .toHaveBeenCalledWith({uploaded : true}, '');
+      afterEach(function() {
+        form.remove();
+        delete window.chrome;
       });
 
-      it('calls the upload method and responds to negative result', function() {
-        const cd = singleDocContentDelegate;
-        const uploadFake = function(pc, pci, h, callback) { callback(false); };
-        spyOn(cd.recap, 'uploadAttachmentMenu').and.callFake(uploadFake);
-        spyOn(cd.notifier, 'showUpload');
-        spyOn(history, 'replaceState');
+      describe('when the history state is already set', function() {
+        beforeEach(function() {
+          history.replaceState({uploaded : true}, '');
+        });
 
-        cd.handleAttachmentMenuPage();
-        expect(cd.recap.uploadAttachmentMenu).toHaveBeenCalled();
-        expect(cd.notifier.showUpload).not.toHaveBeenCalled();
-        expect(history.replaceState).not.toHaveBeenCalled();
+        afterEach(function() {
+          history.replaceState({}, '');
+        });
+
+        it('has no effect', function() {
+          const cd = docketDisplayContentDelegate;
+          spyOn(cd.recap, 'uploadAttachmentMenu');
+          cd.handleAttachmentMenuPage();
+          expect(cd.recap.uploadAttachmentMenu).not.toHaveBeenCalled();
+        });
+      });
+
+      describe('when there is NO appropriate form', function() {
+        it('has no effect when the URL is wrong', function() {
+          const cd = nonsenseUrlContentDelegate;
+          spyOn(cd.recap, 'uploadAttachmentMenu');
+          cd.handleAttachmentMenuPage();
+          expect(cd.recap.uploadAttachmentMenu).not.toHaveBeenCalled();
+        });
+
+        it('has no effect with a proper URL', function() {
+          const cd = singleDocContentDelegate;
+          spyOn(cd.recap, 'uploadAttachmentMenu');
+          cd.handleAttachmentMenuPage();
+          expect(cd.recap.uploadAttachmentMenu).not.toHaveBeenCalled();
+        });
+      });
+
+      describe('when there IS an appropriate form', function() {
+        let input;
+        beforeEach(function() {
+          input = document.createElement('input');
+          input.value = 'Download All';
+          form.appendChild(input);
+        });
+
+        it('has no effect when the URL is wrong', function() {
+          const cd = nonsenseUrlContentDelegate;
+          spyOn(cd.recap, 'uploadAttachmentMenu');
+          cd.handleAttachmentMenuPage();
+          expect(cd.recap.uploadAttachmentMenu).not.toHaveBeenCalled();
+        });
+
+        it('uploads the page when the URL is right', function() {
+          const cd = singleDocContentDelegate;
+          spyOn(cd.recap, 'uploadAttachmentMenu');
+          cd.handleAttachmentMenuPage();
+          expect(cd.recap.uploadAttachmentMenu).toHaveBeenCalled();
+        });
+
+        it('calls the upload method and responds to positive result', function() {
+          const cd = singleDocContentDelegate;
+          const uploadFake = function(pc, pci, h, callback) { callback(true); };
+          spyOn(cd.recap, 'uploadAttachmentMenu').and.callFake(uploadFake);
+          spyOn(cd.notifier, 'showUpload');
+          spyOn(history, 'replaceState');
+
+          cd.handleAttachmentMenuPage();
+          expect(cd.recap.uploadAttachmentMenu).toHaveBeenCalled();
+          expect(cd.notifier.showUpload).toHaveBeenCalled();
+          expect(history.replaceState)
+              .toHaveBeenCalledWith({uploaded : true}, '');
+        });
+
+        it('calls the upload method and responds to negative result', function() {
+          const cd = singleDocContentDelegate;
+          const uploadFake = function(pc, pci, h, callback) { callback(false); };
+          spyOn(cd.recap, 'uploadAttachmentMenu').and.callFake(uploadFake);
+          spyOn(cd.notifier, 'showUpload');
+          spyOn(history, 'replaceState');
+
+          cd.handleAttachmentMenuPage();
+          expect(cd.recap.uploadAttachmentMenu).toHaveBeenCalled();
+          expect(cd.notifier.showUpload).not.toHaveBeenCalled();
+          expect(history.replaceState).not.toHaveBeenCalled();
+        });
       });
     });
   });
