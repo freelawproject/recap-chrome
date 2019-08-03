71f3240aaeb6845d2ad367dc2377eee8a8f637f7
Richard Wright <github@richardowright.com>
2019-08-03 15:12:14 -0400

    basic fix for RECAP fails to load documents for Northern District of Georgia  - #277

--- src/content_delegate.js
+++ src/content_delegate.js
@@ -361,9 +361,20 @@ ContentDelegate.prototype.onDocumentViewSubmit = function (event) {
       // dcd (and presumably others) trigger this code path.
       var reader = new FileReader();
       reader.onload = function() {
+          let html=reader.result;
+          // check if we have an HTML page which redirects the user to the PDF
+          // this was first display by the Northern District of Georgia
+          // https://github.com/freelawproject/recap/issues/277
+          const redirectResult = Array.from(html.matchAll(/(?:window\.location\s=\s")(.*)(?:\")/g));
+          if(redirectResult && redirectResult.length>0) {
+            let url = redirectResult[0][1];
+            html = '<style>body { margin: 0; } iframe { border: none; }' +
+                 '</style><iframe src="' + url +
+                 '" width="100%" height="100%"></iframe>';
+          }
           this.showPdfPage(
-            document.documentElement, reader.result, previousPageHtml,
-            document_number, attachment_number, docket_number);
+              document.documentElement, html, previousPageHtml,
+              document_number, attachment_number, docket_number);
       }.bind(this);
       reader.readAsText(blob);  // convert blob to HTML text
     }
