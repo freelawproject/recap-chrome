3da82a01bb18dc8a56b3028b24db56affae1133a
Eduardo Rosendo <eduardojra96@gmail.com>
2022-12-08 08:13:14 -0400

    fix(appellate): Update logic to use the new Appellate class in content.js

--- src/content.js
+++ src/content.js
@@ -15,13 +15,25 @@ let links = document.body.getElementsByTagName('a');
 
 // seed the content_delegate with the tabId by using the message 
 // returned from the background worker
-function addRecapInformation(msg){
-    // destructure the msg object to get the tabId
-    const { tabId } = msg;
-    
+function addRecapInformation(msg) {
+  // destructure the msg object to get the tabId
+  const { tabId } = msg;
+
+  if (PACER.isAppellateCourt(court)) {
+
+    const AppDelegate = new AppellateDelegate(tabId, court, url, links);
+
+    if (!PACER.hasPacerCookie(document.cookie)) {
+      console.info(`RECAP: Taking no actions because not logged in: ${url}`);
+      return;
+    }
+
+    AppDelegate.dispatchPageHandler();
+  } else {
+
     let content_delegate = new ContentDelegate(tabId,
       url, path, court, pacer_case_id, pacer_doc_id, links);
- 
+
     if (PACER.hasPacerCookie(document.cookie)) {
       // If this is a blank iquery or manage my account page, add RECAP Email advertisement banner.
       content_delegate.addRecapEmailAdvertisement();
@@ -31,44 +43,45 @@ function addRecapInformation(msg){
 
       // If this is a docket query page, ask RECAP whether it has the docket page.
       content_delegate.handleDocketQueryUrl();
-  
+
       // If this is a docket page, upload it to RECAP.
       content_delegate.handleDocketDisplayPage();
-  
+
       // If the page offers the ability to download a zip file, intercept navigation
       // to the post-submit page.
       content_delegate.handleZipFilePageView();
-  
+
       // If this is a document's menu of attachments (subdocuments), upload it to
       // RECAP.
       content_delegate.handleAttachmentMenuPage();
-  
+
       // If this page offers a single document, ask RECAP whether it has the document.
       content_delegate.handleSingleDocumentPageCheck();
-  
+
       // If this page offers a single document, intercept navigation to the document
       // view page.  The "View Document" button calls the goDLS() function, which
       // creates a <form> element and calls submit() on it, so we hook into submit().
       content_delegate.handleSingleDocumentPageView();
-  
+
       // If this is a Clams Register, we upload it to RECAP
       content_delegate.handleClaimsPageView();
-  
+
       // Check every link in the document to see if there is a free RECAP document
       // available. If there is, put a link with a RECAP icon.
       content_delegate.attachRecapLinkToEligibleDocs();
     } else {
       console.info(`RECAP: Taking no actions because not logged in: ${url}`);
+
       let redactionConfirmation = document.getElementById('redactionConfirmation');
-      
+
       let emailInput = document.getElementById('loginForm:loginName')
       let passwordInput = document.getElementById('loginForm:password')
 
-      if (emailInput && passwordInput && redactionConfirmation){
+      if (emailInput && passwordInput && redactionConfirmation) {
         removeFilingState(msg)
       }
 
-      if (redactionConfirmation){
+      if (redactionConfirmation) {
         let redactionObserver = new MutationObserver(mutationRecords => handleRedactionConfirmation(mutationRecords));
         redactionObserver.observe(redactionConfirmation, {
           attributes: true,
@@ -76,6 +89,7 @@ function addRecapInformation(msg){
         });
       }
     }
+  }
 }
 
 // Callback function to execute when mutations in the input with 
