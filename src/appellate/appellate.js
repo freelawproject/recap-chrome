32beb1e14eeb40e99e9f81a5f252b01e57e8f1eb
Eduardo Rosendo <eduardojra96@gmail.com>
2022-12-08 22:58:29 -0400

    style(appellate): Remove white-space and add missing semi-colons

--- src/appellate/appellate.js
+++ src/appellate/appellate.js
@@ -2,8 +2,8 @@
 let AppellateDelegate = function (tabId, court, url, links) {
   this.tabId = tabId;
   this.court = court;
-  this.url = url
-  this.links = links || []
+  this.url = url;
+  this.links = links || [];
   this.recap = importInstance(Recap);
   this.notifier = importInstance(Notifier);
 };
@@ -12,36 +12,35 @@ let AppellateDelegate = function (tabId, court, url, links) {
 // then dispatch the associated handler
 AppellateDelegate.prototype.dispatchPageHandler = function () {
   if (APPELLATE.isCaseSelectionPage(this.url)) {
-    this.handleCaseSelectionPage()
+    this.handleCaseSelectionPage();
   } else {
-    let queryParameters = APPELLATE.getQueryParameters(this.url);    
+    let queryParameters = APPELLATE.getQueryParameters(this.url);
     if (!(queryParameters instanceof URLSearchParams)) {
       return;
     }
 
-    this.queryParameters = APPELLATE.getQueryParameters(this.url);  
+    this.queryParameters = APPELLATE.getQueryParameters(this.url);
     let targetPage = queryParameters.get('servlet');
     switch (targetPage) {
       case 'CaseSummary.jsp':
-        this.handleDocketDisplayPage()
+        this.handleDocketDisplayPage();
         break;
       default:
         console.info('No identified appellate page found');
-        break
+        break;
     }
   }
-
 };
 
 AppellateDelegate.prototype.handleCaseSelectionPage = async function () {
   // retrieve pacer_case_id from the link related to the Case Query
-  let pacer_case_id = APPELLATE.getCaseIdFromCaseSelection()
-  await saveCaseIdinTabStorage({ tabId: this.tabId }, pacer_case_id)
-}
+  let pacer_case_id = APPELLATE.getCaseIdFromCaseSelection();
+  await saveCaseIdinTabStorage({ tabId: this.tabId }, pacer_case_id);
+};
 
 // check every link in the document to see if RECAP has it
-AppellateDelegate.prototype.attachRecapLinksToEligibleDocs= async function () {
-  // filter the links for the documents available on the page 
+AppellateDelegate.prototype.attachRecapLinksToEligibleDocs = async function () {
+  // filter the links for the documents available on the page
   const links = APPELLATE.findDocLinksFromAnchors(this.links);
 
   let linkCount = links.length;
@@ -51,10 +50,13 @@ AppellateDelegate.prototype.attachRecapLinksToEligibleDocs= async function () {
   }
 
   // Ask the server whether any of these documents are available from RECAP.
-  this.recap.getAvailabilityForDocuments(links, this.court,
+  this.recap.getAvailabilityForDocuments(
+    links,
+    this.court,
     $.proxy(function (api_results) {
-      console.info(`RECAP: Got results from API. Running callback on API results to ` +
-        `attach links and icons where appropriate.`);
+      console.info(
+        `RECAP: Got results from API. Running callback on API results to ` + `attach links and icons where appropriate.`
+      );
       for (let i = 0; i < this.links.length; i++) {
         let pacer_doc_id = $(this.links[i]).data('pacer_doc_id');
         if (!pacer_doc_id) {
@@ -68,95 +70,77 @@ AppellateDelegate.prototype.attachRecapLinksToEligibleDocs= async function () {
         }
         let href = `https://storage.courtlistener.com/${result.filepath_local}`;
         let recap_link = $('<a/>', {
-          'class': 'recap-inline',
-          'title': 'Available for free from the RECAP Archive.',
-          'href': href
+          class: 'recap-inline',
+          title: 'Available for free from the RECAP Archive.',
+          href: href,
         });
-        recap_link.append($('<img/>').attr({
-          src: chrome.extension.getURL('assets/images/icon-16.png')
-        }));
+        recap_link.append(
+          $('<img/>').attr({
+            src: chrome.extension.getURL('assets/images/icon-16.png'),
+          })
+        );
         recap_link.insertAfter(this.links[i]);
       }
-      let spinner = document.getElementById("recap-button-spinner")
-      if (spinner){
-          spinner.classList.add('recap-btn-spinner-hidden');
+      let spinner = document.getElementById('recap-button-spinner');
+      if (spinner) {
+        spinner.classList.add('recap-btn-spinner-hidden');
       }
-    }, this));
-}
-
+    }, this)
+  );
+};
 
 AppellateDelegate.prototype.handleDocketDisplayPage = async function () {
   // retrieve pacer_case_id from URL query parameters
-  let pacer_case_id = this.queryParameters.get('caseid') || this.queryParameters.get('caseId')
+  let pacer_case_id = this.queryParameters.get('caseid') || this.queryParameters.get('caseId');
 
-  // if the last step didn't find the caseId in the query parameter, It will check the storage 
+  // if the last step didn't find the caseId in the query parameter, It will check the storage
   if (!pacer_case_id) {
-    const tabStorage = await getItemsFromStorage(this.tabId)
-    if (!tabStorage && !tabStorage.caseId) { return; }
-    pacer_case_id = tabStorage.caseId
+    const tabStorage = await getItemsFromStorage(this.tabId);
+    if (!tabStorage && !tabStorage.caseId) {
+      return;
+    }
+    pacer_case_id = tabStorage.caseId;
   }
 
-  // Query the first table with case data and insert the RECAP actions button 
-  let table = document.querySelectorAll('table')[3]
-  let button = recapActionsButton(this.court, pacer_case_id, false)
-  table.after(button)
+  // Query the first table with case data and insert the RECAP actions button
+  let table = document.querySelectorAll('table')[3];
+  let button = recapActionsButton(this.court, pacer_case_id, false);
+  table.after(button);
 
-  this.recap.getAvailabilityForDocket(
-    this.court,
-    pacer_case_id,
-    (result) => {
-      if (result.count === 0) {
-        console.warn('RECAP: Zero results found for docket lookup.');
-      } else if (result.count > 1) {
-        console.error(
-          `RECAP: More than one result found for docket lookup. Found ${result.count}`
-        );
-      } else {
-        addAlertButtonInRecapAction(this.court, pacer_case_id)
-        let cl_id = getClIdFromAbsoluteURL(result.results[0].absolute_url)
-        addSearchDocketInRecapAction(cl_id)
-      }
+  this.recap.getAvailabilityForDocket(this.court, pacer_case_id, (result) => {
+    if (result.count === 0) {
+      console.warn('RECAP: Zero results found for docket lookup.');
+    } else if (result.count > 1) {
+      console.error(`RECAP: More than one result found for docket lookup. Found ${result.count}`);
+    } else {
+      addAlertButtonInRecapAction(this.court, pacer_case_id);
+      let cl_id = getClIdFromAbsoluteURL(result.results[0].absolute_url);
+      addSearchDocketInRecapAction(cl_id);
     }
-  );
+  });
 
   // if you've already uploaded the page, return
-  if (history.state && history.state.uploaded) { return; }
+  if (history.state && history.state.uploaded) {
+    return;
+  }
 
   const options = await getItemsFromStorage('options');
 
   if (options['recap_enabled']) {
-
     let callback = (ok) => {
       if (ok) {
-        addAlertButtonInRecapAction(this.court, pacer_case_id)
+        addAlertButtonInRecapAction(this.court, pacer_case_id);
         history.replaceState({ uploaded: true }, '');
-        this.notifier.showUpload(
-          'Docket uploaded to the public RECAP Archive.',
-          function () { }
-        );
+        this.notifier.showUpload('Docket uploaded to the public RECAP Archive.', function () {});
       }
     };
 
-    this.recap.uploadDocket(this.court, pacer_case_id,
-      document.documentElement.innerHTML,
-      'APPELLATE_DOCKET',
-      (ok) => callback(ok));
+    this.recap.uploadDocket(this.court, pacer_case_id, document.documentElement.innerHTML, 'APPELLATE_DOCKET', (ok) =>
+      callback(ok)
+    );
 
-    this.attachRecapLinksToEligibleDocs()
-
+    this.attachRecapLinksToEligibleDocs();
   } else {
     console.info(`RECAP: Not uploading docket. RECAP is disabled.`);
   }
-}
-
-
-
-
-
-
-
-
-
-
-
-
+};
