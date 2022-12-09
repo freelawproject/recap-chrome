3db0faa8b8d2b96f44a184dc9ef9ec81b8cdba08
Eduardo Rosendo <eduardojra96@gmail.com>
2022-12-09 14:29:40 -0400

    feat(appellate utils): Update helper methods.
    - Remove the regex expression in the getQueryParameters method. This method will use the search property of the location interface instead.
    - Add log lines in the getCaseIdFromCaseSelection method and remove the comments.
    - Format the module using prettier

--- src/appellate/utils.js
+++ src/appellate/utils.js
@@ -1,13 +1,8 @@
 let APPELLATE = {
-
   // Returns URLSearchParams interface or empty object.
-  getQueryParameters: (url) => {
-    // Use regex expression to get the query string from the page URL 
-    let re = /(?:TransportRoom)\?((&?[\w]+=[^&=\n]+)+)/;
-    let queryParameters = url.match(re);
+  getQueryParameters: () => {
     // Use the URLSearchParams interface to work with the query string of the URL.
-    if (!!queryParameters) return new URLSearchParams(queryParameters[1])
-    return {}
+    return new URLSearchParams(window.location.search);
   },
 
   // Returns true if the URL is for the case selection page.
@@ -19,29 +14,39 @@ let APPELLATE = {
     // but the URL for other pages has a Query string:
     //
     //   https://ecf.ca9.uscourts.gov/n/beam/servlet/TransportRoom?servlet=CaseSummary.jsp&caseNum=20-15019&incOrigDkt=Y&incDktEntries=Y
-    // 
-    // This function checks if the URL has a query string and is related to Appellate PACER. 
+    //
+    // This function checks if the URL has a query string and is related to Appellate PACER.
     // It will return true only when the url match the format of the first example and it will
     // exclude pages that include the TransportRoom word but has a query string like the last example.
-    return /servlet\/TransportRoom/.test(url) && !/[?&]/.test(url)
+    return /servlet\/TransportRoom/.test(url) && !/[?&]/.test(url);
   },
 
   // Returns caseId from href attribute of Case Query link on the Case Selection Page.
   getCaseIdFromCaseSelection: () => {
-    let table = document.querySelectorAll('table')
-    // End this function early if the page does not have the expected number of tables
-    if (table.length < 3) { return }
-    let anchor = table[3].querySelectorAll('tr > td > a')
-    // End this function early if the table does not have the expected number of anchors
-    if (anchor.length < 3) { return }
-    let queryString = anchor[1].href.split('?')[1]
-    let queryParameters = new URLSearchParams(queryString)
-    let caseId = queryParameters.get('caseid') || queryParameters.get('caseId')
+    let table = document.querySelectorAll('table');
 
-    return caseId
+    if (table.length < 3) {
+      console.info(
+        'RECAP: no matching format was detected, the extension will take no action because the page does not have the minimum number of tables.'
+      );
+      return;
+    }
+    let anchor = table[3].querySelectorAll('tr > td > a');
+
+    if (anchor.length < 3) {
+      console.info(
+        "RECAP: no matching format was detected. There aren't enough anchors in the table that the extension found."
+      );
+      return;
+    }
+    let queryString = anchor[1].href.split('?')[1];
+    let queryParameters = new URLSearchParams(queryString);
+    let caseId = queryParameters.get('caseid') || queryParameters.get('caseId');
+
+    return caseId;
   },
 
-  // Create a list of doc_ids from the list of all links available on the page 
+  // Create a list of doc_ids from the list of all links available on the page
   findDocLinksFromAnchors: (nodeList) => {
     const links = [];
     Array.from(nodeList).map((a) => {
@@ -51,5 +56,4 @@ let APPELLATE = {
     });
     return links;
   },
-
-}
+};
