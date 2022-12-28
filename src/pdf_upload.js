0f7d93da7465863077747fc4c0ce8f82f9ec3153
Eduardo Rosendo <eduardojra96@gmail.com>
2022-12-28 07:32:37 -0400

    feat(pdf upload): Add a helper function to show and upload PDF documents

--- src/pdf_upload.js
+++ src/pdf_upload.js
@@ -101,7 +101,6 @@ const handleDocFormResponse = function (type, ab, xhr, previousPageHtml, dataFro
     // canb and ca9 return PDFs and trigger this code path.
     let html = PACER.makeFullPageIFrame(URL.createObjectURL(blob));
     this.showPdfPage(
-      document.documentElement,
       html,
       previousPageHtml,
       dataFromReceipt.doc_number,
@@ -121,7 +120,6 @@ const handleDocFormResponse = function (type, ab, xhr, previousPageHtml, dataFro
         html = PACER.makeFullPageIFrame(url);
       }
       this.showPdfPage(
-        document.documentElement,
         html,
         previousPageHtml,
         dataFromReceipt.doc_number,
@@ -131,4 +129,71 @@ const handleDocFormResponse = function (type, ab, xhr, previousPageHtml, dataFro
     }.bind(this);
     reader.readAsText(blob); // convert blob to HTML text
   }
-}
+};
+
+const showAndUploadPdf = async function (
+  html_elements,
+  previousPageHtml,
+  document_number,
+  attachment_number,
+  docket_number,
+  pacer_doc_id,
+  restricted = false 
+) {
+  // Find the <iframe> URL in the HTML string.
+  let match = html_elements.match(/([^]*?)<iframe[^>]*src="(.*?)"([^]*)/);
+  if (!match) {
+    document.documentElement.innerHTML = html;
+    return;
+  }
+
+  const options = await getItemsFromStorage('options');
+
+  showWaitingMessage(match);
+
+  // Make the Back button redisplay the previous page.
+  window.onpopstate = function (event) {
+    if (event.state.content) {
+      document.documentElement.innerHTML = event.state.content;
+    }
+  };
+  history.replaceState({ content: previousPageHtml }, '');
+
+  let blob = await downloadDataFromIframe(match, this.tabId);
+  let blobUrl = URL.createObjectURL(blob);
+  let pacer_case_id;
+
+  if (attachment_number && PACER.isAppellateCourt(this.court)) {
+    pacer_case_id = this.pacer_case_id
+      ? this.pacer_case_id
+      : await APPELLATE.getCaseId(this.tabId, this.queryParameters, pacer_doc_id);
+  } else {
+    pacer_case_id = this.pacer_case_id
+      ? this.pacer_case_id
+      : await getPacerCaseIdFromPacerDocId(this.tabId, pacer_doc_id);
+  }
+
+  let filename = generateFileName(
+    options,
+    this.court,
+    pacer_case_id,
+    docket_number,
+    document_number,
+    attachment_number
+  );
+  displayPDFOrSaveIt(options, filename, match, blob, blobUrl);
+
+  if (options['recap_enabled'] && !restricted) {
+    // If we have the pacer_case_id, upload the file to RECAP.
+    // We can't pass an ArrayBuffer directly to the background
+    // page, so we have to convert to a regular array.
+    this.recap.uploadDocument(this.court, pacer_case_id, pacer_doc_id, document_number, attachment_number, (ok) => {
+      // callback
+      if (ok) {
+        this.notifier.showUpload('PDF uploaded to the public RECAP Archive.', () => {});
+      }
+    });
+  } else {
+    console.info('RECAP: Not uploading PDF. RECAP is disabled.');
+  }
+};
