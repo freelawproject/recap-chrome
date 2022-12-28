8580f8deebe0333bd47433fb01a4bf3541465fd5
Eduardo Rosendo <eduardojra96@gmail.com>
2022-12-28 00:13:04 -0400

    feat(pdf upload): Add a helper function to handle the document form responses

--- src/pdf_upload.js
+++ src/pdf_upload.js
@@ -89,3 +89,46 @@ const displayPDFOrSaveIt = (options, filename, match, blob, blobUrl) => {
     window.saveAs(blob, filename);
   }
 };
+
+const handleDocFormResponse = function (type, ab, xhr, previousPageHtml, dataFromReceipt) {
+  console.info(`RECAP: Successfully submitted RECAP "View" button form: ${xhr.statusText}`);
+
+  const blob = new Blob([new Uint8Array(ab)], { type: type });
+  // If we got a PDF, we wrap it in a simple HTML page.  This lets us treat
+  // both cases uniformly: either way we have an HTML page with an <iframe>
+  // in it, which is handled by showPdfPage.
+  if (type === 'application/pdf') {
+    // canb and ca9 return PDFs and trigger this code path.
+    let html = PACER.makeFullPageIFrame(URL.createObjectURL(blob));
+    this.showPdfPage(
+      document.documentElement,
+      html,
+      previousPageHtml,
+      dataFromReceipt.doc_number,
+      dataFromReceipt.att_number,
+      dataFromReceipt.docket_number
+    );
+  } else {
+    const reader = new FileReader();
+    reader.onload = function () {
+      let html = reader.result;
+      // check if we have an HTML page which redirects the user to the PDF
+      // this was first display by the Northern District of Georgia
+      // https://github.com/freelawproject/recap/issues/277
+      const redirectResult = Array.from(html.matchAll(/window\.location\s*=\s*["']([^"']+)["'];?/g));
+      if (redirectResult.length > 0) {
+        const url = redirectResult[0][1];
+        html = PACER.makeFullPageIFrame(url);
+      }
+      this.showPdfPage(
+        document.documentElement,
+        html,
+        previousPageHtml,
+        dataFromReceipt.doc_number,
+        dataFromReceipt.att_number,
+        dataFromReceipt.docket_number
+      );
+    }.bind(this);
+    reader.readAsText(blob); // convert blob to HTML text
+  }
+}
