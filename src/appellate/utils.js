821466dc3ba8fbb588c5721739b0f61af9068dd8
John Hawkinson <jhawk@alum.mit.edu>
2023-04-19 22:35:40 -0400

    appellate/utils.js: Use URLSearchParams() properly
    Don't feed the URLSearchParams() constructor an entire URL, it takes only a query string.
    Feeding it a full URL causes it to misparse the first query string, if any, merging it with the URL.
    In practice this was not an actual problem, but could have been.
    
    Similarly, don't set a URL search parameter by concatenating
    "&caseid=xyz"; this fails when there is no preexisting search
    string:(i.e. no ?foobar), and causes the RECAP extension to break
    URLs. Instead, use the URL+URLSearchParamers abstractions.
    
    This has the side effect of converting relative URLs to absolute.  I
    don't think this is an actual problem?
    
    Lastly, don't parse a query string by hand with split('?') -- use the URL constructor to do it.

--- src/appellate/utils.js
+++ src/appellate/utils.js
@@ -151,13 +151,17 @@ let APPELLATE = {
     // us to support Case Selection pages with multiple cases.
 
     document.querySelectorAll('a[href*="caseid"]').forEach((caseQueryAnchor) => {
-      let params = new URLSearchParams(caseQueryAnchor.href);
-      let caseId = params.get('caseId') || params.get('caseid');
+      let queryUrl = new URL(window.location, caseQueryAnchor.href);
+      let queryParams = queryUrl.searchParams;
+      let caseId = queryParams.get('caseId') || queryParams.get('caseid');
       // the Docket Report and the Case Query links are enclosed by the same HTML tag and the anchor for
       // the Docket Report is the first element inside this tag so using the parentElement and the firstChild
       // attribute allow us to get the desired HTML element.
       let caseSummaryAnchor = caseQueryAnchor.parentElement.firstChild;
-      caseSummaryAnchor.setAttribute('href', `${caseSummaryAnchor.href}&caseId=${caseId}`);
+      // This has the side effect of making this URL absolute, when it may have started out relative.
+      summaryUrl = new URL(window.location, caseSummaryAnchor.href);
+      summaryUrl.searchParams.set('caseId', caseId);
+      caseSummaryAnchor.setAttribute('href', summaryURL);
     });
   },
 
@@ -194,8 +198,8 @@ let APPELLATE = {
       return;
     }
 
-    let queryString = anchor[1].href.split('?')[1];
-    let queryParameters = new URLSearchParams(queryString);
+    let url = new URL(window.location, anchor[1].href);
+    let queryParameters = url.searchParams;
     let caseId = queryParameters.get('caseid') || queryParameters.get('caseId');
 
     return caseId;
