f3b20b67ea7013ddf88488f824f5078da1804282
Eduardo Rosendo <eduardojra96@gmail.com>
2024-04-04 18:20:58 -0400

    feat(popup): Updates content of the banner messages and links objects

--- src/options.js
+++ src/options.js
@@ -1,14 +1,14 @@
 // JavaScript for the options page/popup.
 
 const bannerMessages = {
-  A: '',
-  B: '',
+  A: 'The PACER fees class action was settled for $125 million. Free Law Project will continue the fight.',
+  B: 'News! The PACER fees class action was settled. Learn more and help us make PACER free forever.',
 };
 const donateLinks = {
-  A: '',
-  B: '',
-  C: '',
-  D: '',
+  A: 'https://donate.free.law/forms/pacer-a1',
+  B: 'https://donate.free.law/forms/pacer-a2',
+  C: 'https://donate.free.law/forms/pacer-b1',
+  D: 'https://donate.free.law/forms/pacer-b2',
 };
 
 let inputs = document.getElementsByTagName('input');
