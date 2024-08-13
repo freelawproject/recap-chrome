3d5bfa9810414a46eeaf24cf046ca87222fed1f7
Eduardo Rosendo <eduardojra96@gmail.com>
2024-08-13 11:04:50 -0400

    refactor(district): Updates helper method to add recap icons to entries

--- src/content_delegate.js
+++ src/content_delegate.js
@@ -527,9 +527,7 @@ ContentDelegate.prototype.showPdfPage = async function (
 // view page.  The "View Document" button calls the goDLS() function, which
 // creates a <form> element and calls submit() on it, so we hook into submit().
 ContentDelegate.prototype.handleSingleDocumentPageView = function () {
-  if (!PACER.isSingleDocumentPage(this.url, document)) {
-    return;
-  }
+  if (!PACER.isSingleDocumentPage(this.url, document)) return;
 
   if (PACER.hasFilingCookie(document.cookie)) {
     let table = document.querySelector('form > center');
@@ -551,56 +549,64 @@ ContentDelegate.prototype.handleSingleDocumentPageView = function () {
 
   // When we receive the message from the above submit method, submit the form
   // via XHR so we can get the document before the browser does.
-  window.addEventListener('message', this.onDocumentViewSubmit.bind(this), false);
+  window.addEventListener(
+    'message',
+    this.onDocumentViewSubmit.bind(this),
+    false
+  );
 };
 
 // Check every link in the document to see if there is a free RECAP document
 // available. If there is, put a link with a RECAP icon.
-ContentDelegate.prototype.attachRecapLinkToEligibleDocs = function () {
+ContentDelegate.prototype.attachRecapLinkToEligibleDocs = async function () {
   let linkCount = this.pacer_doc_ids.length;
-  console.info(`RECAP: Attaching links to all eligible documents (${linkCount} found)`);
-  if (linkCount === 0) {
-    return;
-  }
+  console.info(
+    'RECAP: Attaching links to all eligible documents ' + `(${linkCount} found)`
+  );
+  if (linkCount === 0) return;
 
   // Ask the server whether any of these documents are available from RECAP.
-  this.recap.getAvailabilityForDocuments(
-    this.pacer_doc_ids,
-    this.court,
-    $.proxy(function (api_results) {
-      console.info(
-        `RECAP: Got results from API. Running callback on API results to ` + `attach links and icons where appropriate.`
-      );
-      for (let i = 0; i < this.links.length; i++) {
-        let pacer_doc_id = $(this.links[i]).data('pacer_doc_id');
-        if (!pacer_doc_id) {
-          continue;
-        }
-        let result = api_results.results.filter(function (obj) {
-          return obj.pacer_doc_id === pacer_doc_id;
-        })[0];
-        if (!result) {
-          continue;
-        }
-        let href = `https://storage.courtlistener.com/${result.filepath_local}`;
-        let recap_link = $('<a/>', {
-          class: 'recap-inline',
-          title: 'Available for free from the RECAP Archive.',
-          href: href,
-        });
-        recap_link.append(
-          $('<img/>').attr({
-            src: chrome.extension.getURL('assets/images/icon-16.png'),
-          })
-        );
-        recap_link.insertAfter(this.links[i]);
-      }
-      let spinner = document.getElementById('recap-button-spinner');
-      if (spinner) {
-        spinner.classList.add('recap-btn-spinner-hidden');
-      }
-    }, this)
+  let clCourt = PACER.convertToCourtListenerCourt(this.court);
+  const recapLinks = await dispatchBackgroundFetch({
+    action: 'getAvailabilityForDocuments',
+    data: {
+      docket_entry__docket__court: clCourt,
+      pacer_doc_id__in: this.pacer_doc_ids.join(','),
+    },
+  });
+  // return if there are no results
+  if (!recapLinks) return;
+
+  console.info(
+    'RECAP: Got results from API. Running callback on API results to ' +
+      'attach links and icons where appropriate.'
   );
+  for (let i = 0; i < this.links.length; i++) {
+    let pacer_doc_id = $(this.links[i]).data('pacer_doc_id');
+    if (!pacer_doc_id) continue;
+
+    let result = recapLinks.results.filter(function (obj) {
+      return obj.pacer_doc_id === pacer_doc_id;
+    })[0];
+    if (!result) continue;
+
+    let href = `https://storage.courtlistener.com/${result.filepath_local}`;
+    let recap_link = $('<a/>', {
+      class: 'recap-inline',
+      title: 'Available for free from the RECAP Archive.',
+      href: href,
+    });
+    recap_link.append(
+      $('<img/>').attr({
+        src: chrome.runtime.getURL('assets/images/icon-16.png'),
+      })
+    );
+    recap_link.insertAfter(this.links[i]);
+  }
+  let spinner = document.getElementById('recap-button-spinner');
+  if (spinner) {
+    spinner.classList.add('recap-btn-spinner-hidden');
+  }
 };
 
 // TODO: Confirm that zip downloading is consistent across jurisdictions
